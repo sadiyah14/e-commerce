4072d7828ca7cdb360278ca67b8062a9175b5694
sadiyah14 <162302475+sadiyah14@users.noreply.github.com>
2024-03-11 00:47:58 +0530

    Update HomaPage.jsx

--- src/customer/pages/HomePage/HomaPage.jsx
+++ src/customer/pages/HomePage/HomaPage.jsx
@@ -1,1 +1,23 @@
 
+import React from 'react'
+import MainCrosel from '../../components/HomeCarosel/MainCrosel'
+import HomeSectionCarosel from '../../components/HomeSectionCarosel/HomeSectionCarousel'
+import { mens_kurta } from '../../../Data/mens_kurta';
+
+const HomePage= () => {
+    return (
+        <div>
+            <MainCrosel/>
+
+            <div className='space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10'>
+               <HomeSectionCarosel data={mens_kurta} sectionName={"Men's Kurta"}/>
+               <HomeSectionCarosel data={mens_kurta} sectionName={"Men's Shoes"}/>
+               <HomeSectionCarosel data={mens_kurta} sectionName={"Men's Shirts"}/>
+               <HomeSectionCarosel data={mens_kurta} sectionName={"Women's Sarees"}/>
+               <HomeSectionCarosel data={mens_kurta} sectionName={"Women's Dress"}/>             
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
